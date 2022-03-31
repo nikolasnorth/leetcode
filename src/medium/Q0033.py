1113271f83fe7b3cd9a5ae961bf79ec92badd582
Nikolas North <nikolasn09@gmail.com>
2022-03-31 10:33:26 -0400

    redo Q33 Search in Rotated Sorted Array

--- src/medium/Q0033.py
+++ src/medium/Q0033.py
@@ -2,37 +2,39 @@
 33. Search in Rotated Sorted Array
 https://leetcode.com/problems/search-in-rotated-sorted-array/
 """
-from math import floor
 from typing import List
 
 
-# O(log n) time, O(1) space, where n is the length of `nums`
+# Time: O(log n), Space: O(1), where n is the length of `nums`
 def search(nums: List[int], target: int):
-    l, r = 0, len(nums) - 1
+    lo, hi = 0, len(nums) - 1
     is_rotated = nums[0] > nums[-1]
     if is_rotated:
-        # Binary search to find index of rotation
-        while l < r:
-            mid = floor(l + (r - l) / 2)
+        # Find index of rotation
+        while lo < hi:
+            mid = lo + (hi - lo) // 2
             if nums[mid] >= nums[0]:
-                l = mid + 1
+                lo = mid + 1
             else:
-                r = mid
+                hi = mid
+        # Reset lo and hi depending on target
+        if target == nums[0]:
+            return 0
         if target < nums[0]:
-            # search right half
-            r = len(nums) - 1
+            # Search right from point of rotation
+            hi = len(nums) - 1
         else:
-            # search left half
-            r = l - 1
-            l = 0
+            # Search left from point of rotation
+            hi = lo - 1
+            lo = 0
     # Binary search to find target
-    while l < r:
-        mid = floor(l + (r - l) / 2)
+    while lo < hi:
+        mid = lo + (hi - lo) // 2
         if nums[mid] == target:
             return mid
         elif nums[mid] > target:
-            r = mid - 1
+            hi = mid - 1
         else:
-            l = mid + 1
+            lo = mid + 1
 
-    return l if nums[l] == target else -1
+    return lo if nums[lo] == target else -1
