b4b61324659e353d987a55dd8150dfa5a1bac8ca
nikolasnorth <nikolasnorth1@gmail.com>
2022-06-24 11:23:07 -0400

    Improve worst-case time complexity of 1480

--- src/easy/Q1480/Q1480.cpp
+++ src/easy/Q1480/Q1480.cpp
@@ -6,12 +6,24 @@
 
 using namespace std;
 
-// Time: O(n), Space: O(n), where n is the size of `nums`.
+// Time: O(n^2), Space: O(n), where n is the size of `nums`.
 vector<int> running_sum(vector<int>& nums) {
   vector<int> sums;
   for (int i = 0; i < nums.size(); ++i) {
     const auto sum = accumulate(nums.cbegin(), nums.cbegin() + i + 1, int{0});
-    sums.push_back(move(sum));
+    sums.push_back(sum);
+  }
+  return sums;
+}
+
+// Time: O(n), Space: O(n), where n is the size of `nums`.
+vector<int> running_sum_2(vector<int>& nums) {
+  if (nums.empty()) return 0;
+  vector<int> sums;
+  int running_sum = nums[0];
+  for (int i = 0; i < sums.size(); ++i) {
+    sums.push_back(running_sum);
+    running_sum += nums[i];
   }
   return sums;
 }
