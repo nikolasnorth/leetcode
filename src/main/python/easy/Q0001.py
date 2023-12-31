a2fabc3b3a8abca4b5d648da5d5879d342bb82d7
nikolasnorth <nikolasnorth1@gmail.com>
2023-12-31 13:15:51 -0500

    Edit 1

--- src/main/python/easy/Q0001.py
+++ src/main/python/easy/Q0001.py
@@ -6,11 +6,10 @@ https://leetcode.com/problems/two-sum/
 
 # Time: O(n), Space: O(n), where n is the length of `nums`.
 def two_sum(nums: list[int], target: int) -> list[int]:
-    diffs = {}
+    visited = {}
     for i, n in enumerate(nums):
-        diffs[target - n] = i
-    for i, n in enumerate(nums):
-        diff_i = diffs.get(n)
-        if diff_i and diff_i != i:
-            return [diff_i, i]
+        diff = target - n
+        if diff in visited:
+            return [i, visited[diff]]
+        visited[n] = i
     return [-1, -1]
