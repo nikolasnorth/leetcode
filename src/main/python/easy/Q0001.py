3ade1623bf1e4ce11362c317fbc36e4ed1fce238
nikolasnorth <nikolasnorth1@gmail.com>
2024-09-25 08:22:01 -0400

    Add naive solution for Q1

--- src/main/python/easy/Q0001.py
+++ src/main/python/easy/Q0001.py
@@ -13,3 +13,14 @@ def two_sum(nums: list[int], target: int) -> list[int]:
             return [i, visited[diff]]
         visited[n] = i
     return [-1, -1]
+
+
+# Time: O(n^2), Space: O(1), where n is the length of nums.
+def two_sum_2(nums: list[int], target: int) -> list[int]:
+    for i, i_val in enumerate(nums):
+        for j, j_val in enumerate(nums):
+            if i == j:
+                continue
+            if i_val + j_val == target:
+                return [i, j]
+    return [-1, -1]
